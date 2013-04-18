d75e43b74496bd8b74d3a4c483adc728d99dbd76
Travis Johnson <traviscj@traviscj.com>
2013-04-18 18:26:01 -0500

    move StoredSession into its own file

---
+++ DropboxStoredSession.py
@@ -0,0 +1,33 @@
+
+from dropbox import session
+# shamelessly stolen from the dropbox examples.
+class StoredSession(session.DropboxSession):
+    """a wrapper around DropboxSession that stores a token to a file on disk"""
+    TOKEN_FILE = "/tmp/token_store.txt"
+
+    def load_creds(self):
+        try:
+            stored_creds = open(self.TOKEN_FILE).read()
+            self.set_token(*stored_creds.split('|'))
+        except IOError:
+            pass # don't worry if it's not there
+
+    def write_creds(self, token):
+        f = open(self.TOKEN_FILE, 'w')
+        f.write("|".join([token.key, token.secret]))
+        f.close()
+
+    def delete_creds(self):
+        os.unlink(self.TOKEN_FILE)
+
+    def link(self):
+        request_token = self.obtain_request_token()
+        url = self.build_authorize_url(request_token)
+        return url
+
+        self.obtain_access_token(request_token)
+        self.write_creds(self.token)
+
+    def unlink(self):
+        self.delete_creds()
+        session.DropboxSession.unlink(self)
--- finance_track.py
+++ finance_track.py
@@ -6,45 +6,14 @@ import sys
 from os.path import expanduser, exists
 from os import unlink
 
-# Include the Dropbox SDK libraries
-from dropbox import client, rest, session
-from os.path import expanduser
-from finance_track_db import *
 
+# Dropbox API keys...
+from finance_track_db import *
+# Include the Dropbox SDK libraries
+from dropbox import client, rest
 # ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
 ACCESS_TYPE = 'app_folder'
-
-# shamelessly stolen from the dropbox examples.
-class StoredSession(session.DropboxSession):
-    """a wrapper around DropboxSession that stores a token to a file on disk"""
-    TOKEN_FILE = "/tmp/token_store.txt"
-
-    def load_creds(self):
-        try:
-            stored_creds = open(self.TOKEN_FILE).read()
-            self.set_token(*stored_creds.split('|'))
-        except IOError:
-            pass # don't worry if it's not there
-
-    def write_creds(self, token):
-        f = open(self.TOKEN_FILE, 'w')
-        f.write("|".join([token.key, token.secret]))
-        f.close()
-
-    def delete_creds(self):
-        os.unlink(self.TOKEN_FILE)
-
-    def link(self):
-        request_token = self.obtain_request_token()
-        url = self.build_authorize_url(request_token)
-        return url
-
-        self.obtain_access_token(request_token)
-        self.write_creds(self.token)
-
-    def unlink(self):
-        self.delete_creds()
-        session.DropboxSession.unlink(self)
+from DropboxStoredSession import StoredSession
 
 class finance_track(object):
 	def __init__(self):
